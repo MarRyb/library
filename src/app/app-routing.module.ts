a11d9c25ee7ae09b0a0bd1b6a716d6d3c50bdd6f
Marina <mg@health24.ua>
2019-07-23 21:47:26 +0300

    added two module (page routing): productsModule and homeModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home', 
+    loadChildren: './home/home.module#HomeModule'
+  },
+  {
+    path: 'products', 
+    loadChildren: './products/products.module#ProductsModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
