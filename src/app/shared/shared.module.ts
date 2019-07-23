0f43d3ba858d4a31a52f9959e04fd7d10deabfe9
Marina <mg@health24.ua>
2019-07-23 22:32:12 +0300

    added style for header and responsive mobile menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 import { RouterModule } from '@angular/router';
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [SidebarComponent, HeaderComponent, FooterComponent, ContentComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CollapseModule.forRoot(), 
+    BsDropdownModule.forRoot()
   ],
   exports: [
     SidebarComponent, HeaderComponent, FooterComponent, ContentComponent
